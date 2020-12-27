478588d7e1105bd3e92b0f3625a3bc2afccf000d
ankitjha02211 <ankitjha02211@gmail.com>
2020-12-28 02:16:14 +0530

    firstPush

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { HeaderComponent } from './homePage/header/header.component';
+import { FooterComponent } from './homePage/footer/footer.component';
+import { HomeComponent } from './homePage/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
